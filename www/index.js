dfd829fe0c1a207092ee2147a3500492271f9006
M. Orçun Topdağı <mot133t@gmail.com>
2015-04-07 16:57:23 +0300

    initial commit

---
+++ www/index.js
@@ -0,0 +1,9 @@
+Reveal.initialize({
+    controls: true,
+    progress: true,
+    mouseWheel: true,
+    transition: 'concave',
+    overview: false,
+    parallaxBackgroundImage: "/img/rugby-673453.jpg",
+    parallaxBackgroundSize: "2100px 1500px"
+});
